fa5741dbde83b716a676e98a32784c96bfdb32be
Julia Gu <juliagu808@gmail.com>
2020-11-13 11:39:54 +0100

    a bit

--- BrandlarmC/Main.c
+++ BrandlarmC/Main.c
@@ -10,16 +10,16 @@ void visaReklam(SERIALPORT port)
 {
 	printf("Restarting ad-lineup.\n");
 	SerialWritePort(port, "a:", strlen("a:"));
-	char buf[512];
-	SerialReadPort(port, buf, 512);
+	/*char buf[512];
+	SerialReadPort(port, buf, 512);*/
 }
 
 void brandlarm(SERIALPORT port)
 {
 	printf("Activating fire emergency protocol.\n");
 	SerialWritePort(port, "b:", strlen("b:"));
-	char buf[512];
-	SerialReadPort(port, buf, 512);
+	/*char buf[512];
+	SerialReadPort(port, buf, 512);*/
 }
 
 void Huvudmeny(SERIALPORT port)
@@ -46,7 +46,7 @@ void Huvudmeny(SERIALPORT port)
 
 int main()
 {
-	SERIALPORT port = SerialInit("\\\\.\\COM3");
+	SERIALPORT port = SerialInit("\\\\.\\COM5");
 	if (!SerialIsConnected(port))
 	{
 		return;
